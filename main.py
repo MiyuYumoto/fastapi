e4d0c7eda6d5de7cca9340d39954ae7fc037dd82
MiyuYumoto <154505101+MiyuYumoto@users.noreply.github.com>
2024-06-20 20:25:15 +0900

    620

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse #インポート
+
 app = FastAPI()
 
 
@@ -12,3 +14,43 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>TDU</title>
+        </head>
+        <body>
+            <h1>東京電機大学の学部紹介</h1>
+            <p>システムデザイン工学部</p>
+            <p>未来科学部</p>
+            <p>工学部</p>
+            <p>理工学部</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.post("/present")
+async def new_naming(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
